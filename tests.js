3f19cec8d68d767f48befa2396568897a11a7cba
pedro <emaildopedromedeiros@gmail.com>
2025-07-30 16:10:56 -0300

    Adiciona metodo a test

--- tests.js
+++ tests.js
@@ -68,3 +68,4 @@ function testConvertObjectComplexNumbersToCoordinates() {
 testGetComplex();
 testGetImageOfGivenDomainNumber();
 testConvertconvertObjectCoordinatesToComplexNumbers();
+testConvertObjectComplexNumbersToCoordinates();
