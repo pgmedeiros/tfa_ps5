9d08c4ea1e8d68550229d01e2f9a2f7f880e2495
pedro <emaildopedromedeiros@gmail.com>
2025-09-26 14:13:26 -0300

    corrige desenho deslocado

--- canva/CanvaMethods.js
+++ canva/CanvaMethods.js
@@ -27,9 +27,11 @@ export function mouse_movement(p5, self, sub, polyy) {
         if(guard(p5)) {
             return;
         }
+
         if (self.dragged === true && p5.pmouseX !== p5.mouseX && p5.pmouseX !== p5.mouseX) {
-            let point = p5.createVector(p5.mouseX, p5.mouseY);
-            let img_p = polyy.getImage(p5.mouseX, p5.mouseY, self.poly_user_input);
+            let position = transform_point(self.slider_scale.value(), p5.mouseX, p5.mouseY);
+            let point = p5.createVector(position.x, position.y);
+            let img_p = polyy.getImage(position.x,  position.y, self.poly_user_input);
             let imgPoint = p5.createVector(img_p.x, img_p.y);
             self.current_line.push(point);
             sub[0].current_line.push(imgPoint);
@@ -48,7 +50,7 @@ export function create_sliders(p5, self) {
     self.slider_line_color_one = p5.createSlider(0, 255, 255);
     self.slider_line_color_two = p5.createSlider(0, 255, 255);
     self.slider_line_color_three = p5.createSlider(0, 255, 255);
-    self.slider_scale = p5.createSlider(5, 20, 10);
+    self.slider_scale = p5.createSlider(1, 50, 10);
     self.slider_line_width = p5.createSlider(1, 5, 3)
 }
 
@@ -78,4 +80,18 @@ function guard(p5) {
     if (p5.pmouseX < 0 || p5.pmouseX >= p5.width || p5.pmouseY < 0 || p5.pmouseY >= p5.height) {
         return true;
     }
+}
+
+function transform_point(scale, x_position, y_position) {
+
+    const inverse_of_scale = get_inverse_of_scale(scale / 10);
+
+    return {
+        x: inverse_of_scale * (x_position - 200),
+        y: inverse_of_scale * (y_position - 200)
+    }
+}
+
+function get_inverse_of_scale(scale) {
+    return 1 / scale;
 }
